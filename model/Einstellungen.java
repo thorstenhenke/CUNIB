ab3e00db3109df22c26da51e4ae222117bf50f51
Thorsten Henke <thorsten.henke@uni-potsdam.de>
2015-04-25 19:56:35 +0200

    Neue Beobachter fuer 2015

--- model/Einstellungen.java
+++ model/Einstellungen.java
@@ -19,7 +19,7 @@ public class Einstellungen
     public static final int KCODESTELLEN        = 4;
 
     public static final String[] BEOBACHTER = {
-            "", "Anja", "Anne", "Annelie", "Christian", "Jennifer", "Sabina", "Sophia", "Steffi", "Thorsten", "Ulrike"
+            "", "Christian", "Jennifer", "Jessica", "Julia", "Nina", "Steffi", "Thorsten"
     };
     public static final String[] UNTERRICHTSSTUNDEN = {
             "", "1. Stunde", "2. Stunde", "3. Stunde", "4. Stunde", "5. Stunde", "6. Stunde"
