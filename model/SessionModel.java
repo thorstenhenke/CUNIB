57865af36aa6b9eb04fe47c26df2e11466d649bd
Thorsten Henke <thorsten.henke@uni-potsdam.de>
2015-04-27 19:35:48 +0200

    Uhrzeit korrekt frmatiert sodass immer 4 stellig

--- model/SessionModel.java
+++ model/SessionModel.java
@@ -65,10 +65,12 @@ public class SessionModel
     {
         Calendar cal = Calendar.getInstance();
 
+        String curTime = String.format("%02d:%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
+
         String rueckgabe = "<Metadaten> \r\n";
 
         rueckgabe += "Datum: " + cal.get(Calendar.DAY_OF_MONTH) + "." + cal.get(Calendar.MONTH) + "." + cal.get(Calendar.YEAR) + "\n\r";
-        rueckgabe += "Uhrzeit: " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + "\n\r";
+        rueckgabe += "Uhrzeit: " + curTime + "\n\r";
         rueckgabe += "Klassenummer: " + klassennummer + "\r\n";
         rueckgabe += "Beobachter: " + beobachter + "\r\n";
         rueckgabe += "Beobachtung: " + beobachterstunde + "\r\n";
