ae92a0d47f74ec3983e7dcd35a73c3e5aa02b1b5
Thorsten Henke <thorsten.henke@uni-potsdam.de>
2015-04-27 19:46:45 +0200

    Kategorie 'Andere Person' hinzugefuegt

--- panels/Erwachsener.java
+++ panels/Erwachsener.java
@@ -13,35 +13,45 @@ public class Erwachsener extends AbstractCustomPanel
 
         setLayout(null);
 
+        // Header
         JLabel lblLernkontext = new JLabel("Der Erwachsene ist ...");
         lblLernkontext.setFont(new Font("Tahoma", Font.BOLD, 17));
         lblLernkontext.setBounds(250, 11, 200, 30);
         lblLernkontext.setHorizontalAlignment(JLabel.CENTER);
         add(lblLernkontext);
 
+        // Erste Spalte
         JButton btnIndividuell = new JButton("1. Lehrkraft");
-        btnIndividuell.setBounds(250, 80, 200, 60);
+        btnIndividuell.setBounds(100, 100, 200, 60);
         btnIndividuell.setActionCommand("l1");
         btnIndividuell.addActionListener(m);
         add(btnIndividuell);
 
         JButton btnGesamteKlasse = new JButton("2. Lehrkraft");
-        btnGesamteKlasse.setBounds(250, 170, 200, 60);
+        btnGesamteKlasse.setBounds(400, 100, 200, 60);
         btnGesamteKlasse.setActionCommand("l2");
         btnGesamteKlasse.addActionListener(m);
         add(btnGesamteKlasse);
 
+        // Zweite Spalte
         JButton btnPartner = new JButton("Sonderpädagoge");
-        btnPartner.setBounds(250, 260, 200, 60);
+        btnPartner.setBounds(100, 220, 200, 60);
         btnPartner.setActionCommand("son");
         btnPartner.addActionListener(m);
         add(btnPartner);
 
-        JButton btnGruppenarbeit = new JButton("Einzelfallhelfer");
-        btnGruppenarbeit.setBounds(250, 350, 200, 60);
-        btnGruppenarbeit.setActionCommand("ein");
-        btnGruppenarbeit.addActionListener(m);
-        add(btnGruppenarbeit);
+        JButton btnEinzelfall = new JButton("Einzelfallhelfer");
+        btnEinzelfall.setBounds(400, 220, 200, 60);
+        btnEinzelfall.setActionCommand("ein");
+        btnEinzelfall.addActionListener(m);
+        add(btnEinzelfall);
+
+        // Footer
+        JButton btnSonstiger = new JButton("Andere Person");
+        btnSonstiger.setBounds(250, 340, 200, 60);
+        btnSonstiger.setActionCommand("and");
+        btnSonstiger.addActionListener(m);
+        add(btnSonstiger);
 
     }
 }
