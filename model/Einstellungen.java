3461fad1939995c28158708107b7e9576042a51b
Thorsten <mail@thorstenluka.de>
2015-10-23 14:40:20 +0200

    Mandy als beobachterin ergaenzt

--- model/Einstellungen.java
+++ model/Einstellungen.java
@@ -19,7 +19,7 @@ public class Einstellungen
     public static final int KCODESTELLEN        = 4;
 
     public static final String[] BEOBACHTER = {
-            "", "Christian", "Jennifer", "Jessica", "Julia", "Nina", "Steffi", "Thorsten"
+            "", "Christian", "Jennifer", "Jessica", "Mandy", "Nina", "Steffi", "Thorsten"
     };
     public static final String[] UNTERRICHTSSTUNDEN = {
             "", "1. Stunde", "2. Stunde", "3. Stunde", "4. Stunde", "5. Stunde", "6. Stunde"
